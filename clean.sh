cd7762720ee54c02c6039ad4c4fbc1374d35f1cb
mfgm2 <repom2@riseup.net>
2021-12-08 07:31:19 +0300

    clean: args

--- clean.sh
+++ clean.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-base=work/base; [ -e "$base" ] && rm -rfv "$base"
-geonames=work/geonames-db; [ -e "$geonames" ] && rm -rfv "$geonames"
+
+for a in "$@"; do
+  case "$a" in
+    "geo") c_geo=1 ;;
+    "nobase") c_nobase=1 ;;
+  esac
+done
+
+base=work/base; [ "$c_nobase" != 1 ] && [ -e "$base" ] && rm -rfv "$base"
+geonames=work/geonames-db; [ "$c_geo" == 1 ] && [ -e "$geonames" ] && rm -rfv "$geonames"
 
