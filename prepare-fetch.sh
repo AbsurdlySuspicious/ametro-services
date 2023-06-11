d874baa1f6467cbda434588ecfb3341ca3437b33
repom2 <repom2@riseup.net>
2023-06-11 19:04:04 +0300

    save installers (or whatever is downloaded)

--- prepare-fetch.sh
+++ prepare-fetch.sh
@@ -4,20 +4,26 @@ source vars || exit 1
 
 [ -e "$EXTRACT_PATH" ] && rm -rf "$EXTRACT_PATH"; mkdir -vp "$EXTRACT_PATH"
 [ -d "$CACHE_PATH" ] || mkdir -vp "$CACHE_PATH"
+[ -d "$INSTALLERS_DIR" ] || mkdir -vp "$INSTALLERS_DIR"
 
 extract_pmetro() {
   file -L "$PMETRO_FILE"
   innoextract -d "$EXTRACT_PATH" "$PMETRO_FILE"
+  extract_result=$?
+  cp -vl "$PMETRO_FILE" "${INSTALLERS_DIR}/downloaded.${1}.bin"
+  return "$extract_result"
 }
 
 if ! [ -f "$PMETRO_FILE" ]; then
+  fetch_idx=0
   for fetch_url in "${PMETRO_URL[@]}"; do
-    echo "Trying '$fetch_url'"
+    (( fetch_idx++ ))
+    echo "Trying '$fetch_url' ($fetch_idx)"
     echo "Downloading pmetro"
     curl "$fetch_url" -vo "$PMETRO_FILE" || continue
 
     echo "Extracting pmetro"
-    extract_pmetro || continue
+    extract_pmetro $fetch_idx || continue
     break
   done
 else
